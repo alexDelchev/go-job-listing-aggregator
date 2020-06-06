c430fe9dcc4bbe8ecdef09006deae5bd4ba5d620
alexDelchev <a.r.delchev@outlook.com>
2020-06-06 23:16:15 +0300

    Define MigrationRunner script and constructing function
    The constructing function also checks if the migration table exists in the given databse and creates it if it doesn't
    exist

--- src/config/database/migration.go
+++ src/config/database/migration.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"database/sql"
+	"log"
 	"time"
 )
 
@@ -10,4 +12,63 @@ type migration struct {
 	Script              string
 	Checksum            string
 	ScriptExecutionTime time.Time
+}
+
+// MigrationRunner reads migration files and executes them
+type MigrationRunner struct {
+	database *sql.DB
+}
+
+// NewMigrationRunner creates the migration table if it does not
+// exist, and returns a new MigrationRunner instance
+func NewMigrationRunner(database *sql.DB) MigrationRunner {
+	mr := MigrationRunner{database: database}
+	mr.ensureVersionTableExistence()
+	return mr
+}
+
+func (mr *MigrationRunner) databaseVersionTableExists() bool {
+	var result bool
+
+	rows, err := mr.database.Query(`
+	SELECT EXISTS (
+		SELECT 
+		FROM 
+			information_schema.tables 
+		WHERE  
+			table_schema = 'public'
+		AND    
+			table_name   = 'database_version'
+	);`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if rows.Next() {
+		rows.Scan(&result)
+	}
+
+	return result
+}
+
+func (mr *MigrationRunner) ensureVersionTableExistence() {
+	if mr.databaseVersionTableExists() {
+		return
+	}
+
+	log.Println("Creating database_version table")
+	_, err := mr.database.Exec(
+		`CREATE TABLE public.database_version(
+			version character varying(50),
+			description character varying(200),
+			script TEXT,
+			checksum character varying(50),
+			script_execution_time timestamp without time zone NOT NULL DEFAULT now(),
+			CONSTRAINT database_version_pk PRIMARY KEY (version)
+		)`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
