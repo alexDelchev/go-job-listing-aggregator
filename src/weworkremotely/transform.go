4f32cfc2437e6bf8d81254e0a283fedb96616f2d
alexDelchev <a.r.delchev@outlook.com>
2020-06-08 00:13:07 +0300

    Add extractTextFromHTML function
    Parses the given text with goquery and returns the resulting document's text

--- src/weworkremotely/transform.go
+++ src/weworkremotely/transform.go
@@ -1,4 +1,31 @@
 package weworkremotely
 
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
 // SourceName defines WeWorkRemotely as the data source
 const SourceName string = "weworkremotely"
+
+func extractTextFromHTML(data string) string {
+	var html string
+
+	// Package text in a html tag if not present
+	if strings.Contains(data, "<html>") {
+		html = data
+	} else {
+		html = fmt.Sprintf("<html><body>%s</body></html>", data)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	if err != nil {
+		log.Println(err)
+		return data
+	}
+
+	return doc.Text()
+}
