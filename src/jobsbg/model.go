34c2c5a6f6e0a35400dfe6ac53c978fab075015c
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 19:02:38 +0300

    Define jobsBGListing struct
    Represents a job listing model from the jobs.bg website

---
+++ src/jobsbg/model.go
@@ -0,0 +1,12 @@
+package jobsbg
+
+type jobsBGListing struct {
+	ID             string
+	Company        string
+	Link           string
+	Title          string
+	PublishingDate string
+	Description    string
+	Location       string
+	Keywords       []string
+}
