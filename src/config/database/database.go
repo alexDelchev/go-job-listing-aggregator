c89c94eb1efe61749e237352d138215bdaa86a0b
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 07:36:26 +0300

    Add Database struct and database connection functions
    The constructing function opens a connection to the database and pi
    ngs it for n retries until it receives an non-error response. After the connection is established the migration runner is invoked

--- src/config/database/database.go
+++ src/config/database/database.go
@@ -1,14 +1,34 @@
 package database
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"path/filepath"
+	"time"
 
-	//PostgreSQL driver
+	// PostgreSQL driver
 	_ "github.com/lib/pq"
 )
+
+// Database contains a *sql.DB pointer and a migration runner
+type Database struct {
+	DB              *sql.DB
+	migrationRunner MigrationRunner
+}
+
+// NewDatabase returns a new Database instance and invokes
+// its migration runner
+func NewDatabase() Database {
+	database := newDB()
+
+	migrationRunner := NewMigrationRunner(database)
+	migrationRunner.ProcessMigrations()
+
+	return Database{DB: database, migrationRunner: migrationRunner}
+}
 
 type databaseSecrets struct {
 	Port     uint16 `json:"db_port"`
@@ -36,4 +56,41 @@ func loadSecrets() databaseSecrets {
 	}
 
 	return secrets
+}
+
+func ping(db *sql.DB, retries uint8) {
+	success := false
+
+	for i := uint8(0); i < retries && !success; i++ {
+		if err := db.Ping(); err != nil {
+			log.Printf("Failed to ping database, retrying in 500 ms: %s", err.Error())
+			time.Sleep(500 * time.Millisecond)
+		} else {
+			success = true
+		}
+	}
+
+	if !success {
+		log.Panicf("Failed to successfully ping database after %d retries", retries)
+	}
+}
+
+func newDB() *sql.DB {
+	secrets := loadSecrets()
+	connectionString := fmt.Sprintf(
+		"port=%d host=%s user=%s password=%s dbname=%s sslmode=%s",
+		secrets.Port, secrets.Host, secrets.User, secrets.Password, secrets.Name, secrets.SSLMode)
+
+	log.Printf("Connecting to database %s:%d \n", secrets.Host, secrets.Port)
+
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		panic(err)
+	}
+
+	ping(db, 30)
+
+	log.Printf("Successfully connected to database %s:%d \n", secrets.Host, secrets.Port)
+
+	return db
 }
