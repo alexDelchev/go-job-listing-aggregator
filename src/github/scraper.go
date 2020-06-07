dfab7562d6b53b54e3abff2b6a03dae164ff7462
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 18:29:05 +0300

    Add Scrape function
    Takes a Query model and fetches tthe resulting jobPositionAPIModels for the Query's keywords and location properties. The model slice is stranformed to a slice of listing.Listing after wich listingService.CreateListings is called

--- src/github/scraper.go
+++ src/github/scraper.go
@@ -3,6 +3,7 @@ package github
 import (
 	"go-job-listing-aggregator/src/listing"
 	"go-job-listing-aggregator/src/query"
+	"log"
 )
 
 // Scraper contains a listing service and query service
@@ -14,4 +15,17 @@ type Scraper struct {
 // NewScraper returns a new instance
 func NewScraper(listingService listing.Service, queryService query.Service) Scraper {
 	return Scraper{listingService: listingService, queryService: queryService}
+}
+
+// Scrape fetches job listings from github for the given Query
+func (s *Scraper) Scrape(searchQuery query.Query) {
+	log.Printf("Starting for Query %+v", searchQuery)
+
+	results, err := searchPositions(searchQuery.Keywords, searchQuery.Location)
+	if err != nil {
+		return
+	}
+	resultsTransformed := transformToListingModelSlice(searchQuery.ID, results)
+
+	s.listingService.CreateListings(resultsTransformed)
 }
