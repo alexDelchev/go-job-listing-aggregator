dd70510b96a94b0101caf59c58446bf040f5dc95
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 16:59:43 +0300

    Add writeResponse utility function
    Marshals the data paramater into a json string, writer a status code 200, a Content-type header application/json and writes the json string as the response body

--- src/listing/controller.go
+++ src/listing/controller.go
@@ -1,6 +1,10 @@
 package listing
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -13,4 +17,17 @@ func newController(service Service, router *mux.Router) controller {
 	newController := controller{service: service, router: router}
 
 	return newController
+}
+
+func writeResponse(writer http.ResponseWriter, data interface{}, code int) {
+	responseBody, err := json.Marshal(data)
+	if err != nil {
+		log.Println(err)
+		http.Error(writer, err.Error(), code)
+		return
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	writer.Header().Add("Content-type", "application/json")
+	writer.Write(responseBody)
 }
