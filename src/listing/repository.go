0d263f7013128b6592e1b214f0dc9b2531bbe556
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 16:18:16 +0300

    Define repository interface

---
+++ src/listing/repository.go
@@ -0,0 +1,17 @@
+package listing
+
+type repository interface {
+	getListingByID(id uint64) (Listing, error)
+
+	getListingsByQueryIDAndSourceName(queryID uint64, sourceName string) ([]Listing, error)
+
+	getListingsByQueryID(queryID uint64) ([]Listing, error)
+
+	getLatestListingsBySourceName(sourceName string, limit uint16) ([]Listing, error)
+
+	getSourceNames() ([]string, error)
+
+	listingExists(externalID string, sourceName string) (bool, error)
+
+	insertListing(listing *Listing) (uint64, error)
+}
