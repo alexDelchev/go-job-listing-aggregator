88d6be176da9774b0a6dd3b86ddef1649127ed8c
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 18:43:32 +0300

    Add controller struct and constructing function

---
+++ src/github/controller.go
@@ -0,0 +1,16 @@
+package github
+
+import (
+	"github.com/gorilla/mux"
+)
+
+type controller struct {
+	scheduler *Scheduler
+	router    *mux.Router
+}
+
+func newContoller(scheduler *Scheduler, router *mux.Router) controller {
+	newController := controller{scheduler: scheduler, router: router}
+
+	return newController
+}
