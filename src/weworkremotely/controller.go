1ed4b2f638d9c141684f069db8ddb6748a3652e0
alexDelchev <a.r.delchev@outlook.com>
2020-06-08 00:37:27 +0300

    Add stopScheduler handler function
    Maps to /modules/weworkremotely/scheduler/stop with http method DELETE

--- src/weworkremotely/controller.go
+++ src/weworkremotely/controller.go
@@ -15,11 +15,19 @@ func newContoller(scheduler *Scheduler, router *mux.Router) controller {
 	newController := controller{scheduler: scheduler, router: router}
 
 	router.HandleFunc("/modules/weworkremotely/scheduler/start", newController.startScheduler).Methods("POST")
+	router.HandleFunc("/modules/weworkremotely/scheduler/stop", newController.stopScheduler).Methods("DELETE")
+
 	return newController
 }
 
 func (c *controller) startScheduler(writer http.ResponseWriter, request *http.Request) {
 	c.scheduler.Start()
+
+	writer.WriteHeader(http.StatusAccepted)
+}
+
+func (c *controller) stopScheduler(writer http.ResponseWriter, request *http.Request) {
+	c.scheduler.Stop()
 
 	writer.WriteHeader(http.StatusAccepted)
 }
