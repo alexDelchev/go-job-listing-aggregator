35b0a8c725ea83bf629457cfeceb1942336849f8
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 23:37:45 +0300

    Add TestTransformToListingModel test
    Tests that data is properly mapped to a listing.Listing struct

--- src/stackoverflow/transform_test.go
+++ src/stackoverflow/transform_test.go
@@ -3,6 +3,9 @@
 package stackoverflow
 
 import (
+	"fmt"
+	"go-job-listing-aggregator/src/listing"
+	"go-job-listing-aggregator/src/testutils"
 	"testing"
 )
 
@@ -17,4 +20,31 @@ func TestExtractTextFromHTML(t *testing.T) {
 		t.Errorf("%s failed: Expected output %s, got %s",
 			t.Name(), expectedOutput, actualOutput)
 	}
+}
+
+func TestTransformToListingModel(t *testing.T) {
+	model := jobListingRSSModel{
+		ID:          "id",
+		Link:        "link",
+		Title:       "title",
+		AuthorName:  "author_name",
+		Categories:  []string{"value_1", "value_2", "value_3"},
+		Description: "description"}
+
+	const queryID uint64 = 1
+
+	expectedListing := listing.Listing{
+		ExternalID:  "id",
+		Link:        "link",
+		Name:        "title",
+		Company:     "author_name",
+		Keywords:    []string{"value_1", "value_2", "value_3"},
+		Description: "description",
+		QueryID:     queryID,
+		SourceName:  SourceName}
+
+	actualListing := transformToListingModel(queryID, &model)
+
+	errorMessagePrefix := fmt.Sprintf("%s failed:", t.Name())
+	testutils.CompareListings(t, expectedListing, actualListing, errorMessagePrefix)
 }
