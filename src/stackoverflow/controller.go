69f8dfa248d7db989ffc8f4f5b7d5ab269c0092c
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 23:59:46 +0300

    Add stopScheduler handler function
    Maps to /modules/stackoverflow/scheduler/stop for http method DELETE

--- src/stackoverflow/controller.go
+++ src/stackoverflow/controller.go
@@ -15,12 +15,19 @@ func newContoller(scheduler *Scheduler, router *mux.Router) controller {
 	newController := controller{scheduler: scheduler, router: router}
 
 	router.HandleFunc("/modules/stackoverflow/scheduler/start", newController.startScheduler).Methods("POST")
+	router.HandleFunc("/modules/stackoverflow/scheduler/stop", newController.stopScheduler).Methods("DELETE")
 
 	return newController
 }
 
 func (c *controller) startScheduler(writer http.ResponseWriter, request *http.Request) {
 	c.scheduler.Start()
+
+	writer.WriteHeader(http.StatusAccepted)
+}
+
+func (c *controller) stopScheduler(writer http.ResponseWriter, request *http.Request) {
+	c.scheduler.Stop()
 
 	writer.WriteHeader(http.StatusAccepted)
 }
