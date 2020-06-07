121ba604265e386edaafda80b4f84f34307c16e9
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 18:41:10 +0300

    Add schedule function
    Starts a ticker for the given duration and a stopChannel. Invokes the given function on every heartbeat until the stop channel is written into

--- src/github/scheduling.go
+++ src/github/scheduling.go
@@ -1,9 +1,34 @@
 package github
 
+import (
+	"time"
+)
+
 // Scheduler contains a pointer to a Scraper struct
 // and a stop channel which is used to stop the
 // scheduled execution of Scraper.Scrape.
 type Scheduler struct {
 	Scraper     *Scraper
 	stopChannel chan<- bool
+}
+
+type task func()
+
+func schedule(action task, interval time.Duration) chan<- bool {
+	ticker := time.NewTicker(interval)
+	quit := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				action()
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+
+	return quit
 }
