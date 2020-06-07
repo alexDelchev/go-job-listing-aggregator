63b25a3953cf84d6d1d9742aba16ac7aaccf45c1
alexDelchev <a.r.delchev@outlook.com>
2020-06-08 00:33:19 +0300

    Add schedule function
    Starts a go routine which calls the given function at a fixed interval. Returns a channel which can be written into to stop the scheduling.

--- src/weworkremotely/scheduling.go
+++ src/weworkremotely/scheduling.go
@@ -1,8 +1,33 @@
 package weworkremotely
 
+import (
+	"time"
+)
+
 // Scheduler calls Service.RunForActiveQueries at a given
-// interval.
+// interval/
 type Scheduler struct {
 	Scraper     *Scraper
 	stopChannel chan<- bool
+}
+
+type task func()
+
+func schedule(action task, interval time.Duration) chan<- bool {
+	ticker := time.NewTicker(interval)
+	quit := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				action()
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+
+	return quit
 }
