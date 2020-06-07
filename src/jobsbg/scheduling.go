d0435cad73c831e89346f0d1c8d3cecfb8e36ad3
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 22:41:48 +0300

    Add schedule function
    Starts a new go routine which invokes the supplied function every time interval. Returns a channel which can be written into to stop the scheduled execution of the given function

--- src/jobsbg/scheduling.go
+++ src/jobsbg/scheduling.go
@@ -1,8 +1,33 @@
 package jobsbg
 
+import (
+	"time"
+)
+
 // Scheduler executes Service.RunForActiveQueries at a given time period.Scheduler
 // This scheduled execution can be also be started/stopped.
 type Scheduler struct {
 	Scraper     *Scraper
 	stopChannel chan<- bool
+}
+
+type task func()
+
+func schedule(action task, interval time.Duration) chan<- bool {
+	ticker := time.NewTicker(interval)
+	quit := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				action()
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+
+	return quit
 }
