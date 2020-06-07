defb7ccbb08dd68b61b8543b5f971efddb1e8410
alexDelchev <a.r.delchev@outlook.com>
2020-06-08 00:24:08 +0300

    Add containsAnyString function
    Returns whether the given string contains any of the given strings

--- src/weworkremotely/scraper.go
+++ src/weworkremotely/scraper.go
@@ -3,6 +3,7 @@ package weworkremotely
 import (
 	"go-job-listing-aggregator/src/listing"
 	"go-job-listing-aggregator/src/query"
+	"strings"
 )
 
 // Scraper loads the WeWorkRemotely rss feed and filters out
@@ -16,4 +17,14 @@ type Scraper struct {
 // NewScraper returns a new intance.
 func NewScraper(listingService listing.Service, queryService query.Service) Scraper {
 	return Scraper{listingService: listingService, queryService: queryService}
+}
+
+func containsAnyString(text string, tokens []string) bool {
+	for _, token := range tokens {
+		if strings.Contains(text, token) {
+			return true
+		}
+	}
+
+	return false
 }
