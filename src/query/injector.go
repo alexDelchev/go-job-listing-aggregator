dfe5d32f14c1142120c7574970a36b3e588874ef
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 16:03:45 +0300

    Add NewDefaultModule function
    NewDefaultModule returns a new Module instance where the repository, Service, and contoller have the default dependencies injected

--- src/query/injector.go
+++ src/query/injector.go
@@ -1,9 +1,26 @@
 package query
 
+import (
+	"database/sql"
+
+	"github.com/gorilla/mux"
+)
+
 // Module contains a query repository, Service, controller,
 // with the service being exported.
 type Module struct {
 	repository repository
 	Service    Service
 	controller controller
+}
+
+// NewDefaultModule returns a new Module instance where the,
+// repository, Service, and contoller have the default dependencies
+// injected.
+func NewDefaultModule(database *sql.DB, router *mux.Router) Module {
+	repository := newRepository(database)
+	service := NewService(repository)
+	controller := newController(service, router)
+
+	return Module{repository: repository, Service: service, controller: controller}
 }
