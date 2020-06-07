4c3a9d8b2ec0e7bf4e1cb1e601730880a8e8030d
alexDelchev <a.r.delchev@outlook.com>
2020-06-08 00:08:43 +0300

    Add jobsRssFeed struct

--- src/weworkremotely/model.go
+++ src/weworkremotely/model.go
@@ -1,5 +1,7 @@
 package weworkremotely
 
+import "encoding/xml"
+
 type jobListingRSSModel struct {
 	ID          string `xml:"guid"`
 	Title       string `xml:"title"`
@@ -13,4 +15,9 @@ type jobsRSSChannel struct {
 	Link             string               `xml:"link"`
 	Description      string               `xml:"decription"`
 	PositionListings []jobListingRSSModel `xml:"item"`
+}
+
+type jobsRSSFeed struct {
+	XMLName xml.Name       `xml:"rss"`
+	Channel jobsRSSChannel `xml:"channel"`
 }
