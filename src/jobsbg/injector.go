eac6c653e079c5508ebc5aeff8ff2ef2a11b18e9
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 23:12:53 +0300

    Add NewDefaultModule function
    NewDefaultModule injects the default dependencies.

--- src/jobsbg/injector.go
+++ src/jobsbg/injector.go
@@ -1,9 +1,29 @@
 package jobsbg
 
-// Module contains a Scraper, Scheduler, and a controller instance,
-// with the Scheduler and Scraper exported.
+import (
+	"go-job-listing-aggregator/src/listing"
+	"go-job-listing-aggregator/src/query"
+
+	"github.com/gorilla/mux"
+)
+
+//Module contains a Scraper, Scheduler, and a controller instance,
+// with the Scheduler and Scraper exported."
 type Module struct {
 	Scraper    Scraper
 	Scheduler  Scheduler
 	controller controller
+}
+
+// NewDefaultModule injects the default dependencies.
+func NewDefaultModule(
+	listingService listing.Service,
+	queryService query.Service,
+	router *mux.Router) Module {
+
+	scraper := NewScraper(listingService, queryService)
+	scheduler := NewScheduler(&scraper)
+	controller := newContoller(&scheduler, router)
+
+	return Module{Scraper: scraper, Scheduler: scheduler, controller: controller}
 }
