364d27795beede70cff1b27e830d8d4dcaa6e6cf
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 18:06:21 +0300

    Add extractTextFromHTML function
    Takes a string and parses it with goquery, returns the resulting document text

--- src/github/transform.go
+++ src/github/transform.go
@@ -1,4 +1,31 @@
 package github
 
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
 // SourceName defines github as the data source
 const SourceName string = "github"
+
+func extractTextFromHTML(data string) string {
+	var html string
+
+	// Package text in a html tag if not present
+	if strings.Contains(data, "<html>") {
+		html = data
+	} else {
+		html = fmt.Sprintf("<html><body>%s</body></html>", data)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	if err != nil {
+		log.Println(err)
+		return data
+	}
+
+	return doc.Text()
+}
