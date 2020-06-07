f038ad9e7aef73adacd273693c767bf42649c0c2
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 16:57:36 +0300

    Define controller struct and constructing g fucntion
    The struct contains a Service instance and a pointer to a mux.Router instance for request routing

---
+++ src/listing/controller.go
@@ -0,0 +1,16 @@
+package listing
+
+import (
+	"github.com/gorilla/mux"
+)
+
+type controller struct {
+	service Service
+	router  *mux.Router
+}
+
+func newController(service Service, router *mux.Router) controller {
+	newController := controller{service: service, router: router}
+
+	return newController
+}
