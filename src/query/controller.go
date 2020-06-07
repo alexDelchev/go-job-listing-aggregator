1b73a6905d1d7693c949663ae2c573da71cd15a6
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 15:11:57 +0300

    Add getQueryByID handler function
    Maps to the /query path, for the http method GET

--- src/query/controller.go
+++ src/query/controller.go
@@ -1,6 +1,11 @@
 package query
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
@@ -12,5 +17,36 @@ type controller struct {
 func newController(service Service, router *mux.Router) controller {
 	newController := controller{service: service, router: router}
 
+	router.HandleFunc("/queries", newController.getQueryByID).Methods("GET")
+
 	return newController
+}
+
+func writeResponse(writer http.ResponseWriter, data interface{}, code int) {
+	responseBody, err := json.Marshal(data)
+	if err != nil {
+		log.Println(err)
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	writer.WriteHeader(code)
+	writer.Header().Add("Content-type", "application/json")
+	writer.Write(responseBody)
+}
+
+func (c *controller) getQueryByID(writer http.ResponseWriter, request *http.Request) {
+	idString := request.URL.Query().Get("id")
+	queryID, err := strconv.ParseUint(idString, 0, 64)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	query, err := c.service.GetQueryByID(queryID)
+	if err != nil {
+		writeResponse(writer, err.Error, http.StatusInternalServerError)
+	}
+
+	writeResponse(writer, query, http.StatusOK)
 }
