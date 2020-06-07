0a633ce4e8188b6a2433912bf16555ba75330139
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 19:10:25 +0300

    Add transformToListingModel function
    Transforms a jobsBGListing struct to a listing.Listing struct

--- src/jobsbg/transform.go
+++ src/jobsbg/transform.go
@@ -1,4 +1,22 @@
 package jobsbg
 
+import (
+	"go-job-listing-aggregator/src/listing"
+)
+
 // SourceName defines the source as jobsbg
 const SourceName = "jobsbg"
+
+func transformToListingModel(queryID uint64, jobsBGListing *jobsBGListing) listing.Listing {
+	return listing.Listing{
+		ExternalID:  jobsBGListing.ID,
+		Link:        jobsBGListing.Link,
+		Name:        jobsBGListing.Title,
+		Company:     jobsBGListing.Company,
+		Location:    jobsBGListing.Location,
+		PostingDate: jobsBGListing.PublishingDate,
+		Description: jobsBGListing.Description,
+		Keywords:    jobsBGListing.Keywords,
+		QueryID:     queryID,
+		SourceName:  SourceName}
+}
