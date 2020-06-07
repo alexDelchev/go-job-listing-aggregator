69d5e63b954fe4017baadf5466f9b02ec93a86b2
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 19:20:03 +0300

    Add getListingDescription function
    Loads the page for the given url and returns the nested listing description using goquery

--- src/jobsbg/scraper.go
+++ src/jobsbg/scraper.go
@@ -3,6 +3,10 @@ package jobsbg
 import (
 	"go-job-listing-aggregator/src/listing"
 	"go-job-listing-aggregator/src/query"
+	"log"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 const domain = "https://www.jobs.bg/"
@@ -18,4 +22,24 @@ type Scraper struct {
 // NewScraper returns a new instance.
 func NewScraper(listingService listing.Service, queryService query.Service) Scraper {
 	return Scraper{listingService: listingService, queryService: queryService}
+}
+
+func getListingDescription(url string) string {
+	html, err := getListingPage(url)
+	if err != nil {
+		var result string
+		return result
+	}
+
+	document, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	if err != nil {
+		log.Println(err)
+		var result string
+		return result
+	}
+
+	document.Find("script").Remove()
+	tables := document.Find("body > table > tbody > tr > td > table")
+
+	return goquery.NewDocumentFromNode(tables.Get(1)).Text()
 }
