1ac3f767a514ff166faf11f3b26537fdace8947c
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 23:53:31 +0300

    Add schedule function
    Starts a new go routine which invokes the given function at the given interval and returns a channel which can be written into to stop the scheduled execution

--- src/stackoverflow/scheduling.go
+++ src/stackoverflow/scheduling.go
@@ -1,8 +1,33 @@
 package stackoverflow
 
+import (
+	"time"
+)
+
 // Scheduler calls Scraper.RunForActiveQueries at a
 // given interval.
 type Scheduler struct {
 	Scraper     *Scraper
 	stopChannel chan<- bool
+}
+
+type task func()
+
+func schedule(action task, interval time.Duration) chan<- bool {
+	ticker := time.NewTicker(interval)
+	quit := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				action()
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+
+	return quit
 }
