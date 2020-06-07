cdf75e364fbf0e0d706051cd254e4f805efe1134
alexDelchev <a.r.delchev@outlook.com>
2020-06-07 08:13:27 +0300

    Define query.Service interface
    Implementations of this interface will call the repository

---
+++ src/query/service.go
@@ -0,0 +1,19 @@
+package query
+
+type Service interface {
+	GetQueryByID(id uint64) (Query, error)
+
+	GetActiveQueries() ([]Query, error)
+
+	GetInactiveQueries() ([]Query, error)
+
+	GetAllQueries() ([]Query, error)
+
+	CreateQuery(query Query) (Query, error)
+
+	UpdateQuery(query Query) (Query, error)
+
+	ActivateQuery(id uint64) (Query, error)
+
+	DeactivateQuery(id uint64) (Query, error)
+}
