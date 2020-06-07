16729d7edbdc2c6fcd073962c9dc1ccf1af56231
alexDelchev <a.r.delchev@outlook.com>
2020-06-08 00:20:03 +0300

    Add TestTransformToListingModel test
    Test that data is properly mapped to a listing.Listing struct

--- src/weworkremotely/transform_test.go
+++ src/weworkremotely/transform_test.go
@@ -3,6 +3,9 @@
 package weworkremotely
 
 import (
+	"fmt"
+	"go-job-listing-aggregator/src/listing"
+	"go-job-listing-aggregator/src/testutils"
 	"testing"
 )
 
@@ -36,4 +39,31 @@ func TestDeconstructTitle(t *testing.T) {
 		t.Errorf("%s failed: Expected sconed value %s, got %s",
 			t.Name(), expectedSecondValue, actualSecondValue)
 	}
+}
+
+func TestTransformToListingModel(t *testing.T) {
+	model := jobListingRSSModel{
+		ID:          "id",
+		Link:        "link",
+		Title:       "company: value1 value2",
+		PubDate:     "publish_date",
+		Description: "description"}
+
+	const queryID uint64 = 1
+
+	expectedListing := listing.Listing{
+		ExternalID:  "id",
+		Link:        "link",
+		Name:        "value1 value2",
+		Company:     "company",
+		Keywords:    []string{"value1", "value2"},
+		PostingDate: "publish_date",
+		Description: "description",
+		QueryID:     queryID,
+		SourceName:  SourceName}
+
+	actualListing := transformToListingModel(queryID, &model)
+
+	errorMessagePrefix := fmt.Sprintf("%s failed:", t.Name())
+	testutils.CompareListings(t, expectedListing, actualListing, errorMessagePrefix)
 }
